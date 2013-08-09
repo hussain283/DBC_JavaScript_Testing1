d98c5488a84724058e45bd40e5117eb8d686abe8
Jared Grippe <jared@deadlyicon.com>
2013-08-09 16:40:21 -0700

    WIP

--- person.js
+++ person.js
@@ -2,6 +2,6 @@ function Person(name){
   this.name = name;
 }
 
-
-
-
+Person.prototype.scream = function() {
+  return "MY NAME IS " + this.name.toUpperCase() + "!";
+};
--- person.tests.js
+++ person.tests.js
@@ -13,4 +13,9 @@ assert(jared.scream() === 'MY NAME IS JARED!', 'jared failed to scream');
 assert(jeffrey.scream() === 'MY NAME IS JEFFREY!', 'jeffrey failed to scream');
 assert(jeffrey.scream === jared.scream, 'jared.scream and jeffrey.scream should be the same');
 
+jeffrey.procreate();
+
+assert(jeffrey.children.length === 1, 'jeffrey should have 1 child');
+assert(jeffrey.childre[0] instanceof Person, 'jeffrey\'s kid should be a child');
+
 console.log('all tests pass!');
