76c070dba6f9f859dc51ef5a8eafbac8c241c5bb
Jared Grippe <jared@deadlyicon.com>
2013-08-09 17:14:23 -0700

    update

--- person.tests.js
+++ person.tests.js
@@ -7,6 +7,9 @@ function assert(result, error){
 var jared  = new Person({name: 'Jared', sex:'male'});
 var alice  = new Person({name: 'Alice', sex:'female'});
 
+
+assert(jared instanceof Person, 'jared should be a Person');
+assert(alice instanceof Person, 'alice should be a Person');
 assert(jared.name === 'Jared', 'name should be Jared');
 assert(alice.name === 'Alice', 'name should be Jeffrey');
 
@@ -49,6 +52,12 @@ sparky = jared.buyPet(Dog, {name:'Sparky', sex:'male'});
 
 assert(jared.pets.length === 1, 'jared should have 1 pet');
 assert(jared.pets[0] === sparky, 'jared\'s pet should be sparky');
+
+assert(!(sparky instanceof Person), 'sparky should not be a Person');
+assert(sparky instanceof Dog, 'sparky should be a Dog');
+assert(!(jared instanceof Dog), 'jared should not be a Dog');
+assert(!(alice instanceof Dog), 'alice should not be a Dog');
+
 assert(sparky.name === 'Sparky', 'sparky should be named Sparky');
 assert(sparky.sex === 'male', 'sparky should be male');
 assert(sparky.owner === jared, 'sparky\'s owner should be jared');
