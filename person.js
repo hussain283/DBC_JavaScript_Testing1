d6754099a8c17e3766f7a0f354615b34d8666f2d
Hussain Tambawala <hussain283@gmail.com>
2013-08-13 13:59:53 -0700

    First Commit

--- person.js
+++ person.js
@@ -1,25 +1,68 @@
-function Person(options){
+function Mammal(options){
   this.name = options.name;
   this.sex = options.sex;
   this.children = [];
+  this.alive = true;
 }
+
+Mammal.prototype.kill = function() {
+  this.alive = false;
+}
+
+Mammal.prototype.procreateWith = function(partner, options) {
+  
+  if (partner === this) { throw 'you cannot have a baby with yourself!' }
+
+  if (partner.sex === 'male') { throw 'two men cannot have a baby!' }
+
+  var child = new this.constructor(options)
+  this.children.push(child);
+  partner.children.push(child)
+
+  if (this.sex === 'male') {
+    child.mother = this.wife
+    child.father = this
+  } else {
+    child.mother = this
+    child.father = this.husband
+  } 
+  return child;
+}
+
+function Dog(options, owner) {
+  Mammal.call(this, options);
+  this.owner = owner;
+}
+
+Dog.prototype = Object.create(Mammal.prototype);
+Dog.prototype.constructor = Dog;
+
+function Person(options){
+  Mammal.call(this,options);
+  this.pets = []
+  this.alive = true;
+}
+
+Person.prototype = Object.create(Mammal.prototype);
+Person.prototype.constructor = Person;
 
 Person.prototype.scream = function() {
   return "MY NAME IS " + this.name.toUpperCase() + "!";
 };
 
-Person.prototype.procreate = function() {
-  this.children.push(new Person);
-  return this;
+Person.prototype.wed = function(spouse) {
+  if (this.sex == 'male') {
+    this.wife = spouse;
+    spouse.husband = this;
+  }
+  else {
+    this.husband = spouse;
+    spouse.wife = this;
+  }
 }
 
-Person.prototype.wed = function(spouse) {
-	if (this.sex == 'male') {
-		this.wife = spouse;
-		spouse.husband = this;
-	}
-	else {
-		this.husband = spouse;
-		spouse.wife = this;
-	}
+Person.prototype.buyPet = function(klass, options) {
+  pet = new klass(options, this)
+  this.pets.push(pet)
+  return pet
 }
